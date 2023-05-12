c1bf7777d48af7d2549d5a017a66eec895f4fbc1
Gabriel Januário <gabrielojc@gmail.com>
2023-05-12 00:57:32 -0300

    [WMP-0000] Adding path alias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ComponentsModule } from '@components/components.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentsModule } from './shared/components/components.module';
 
 @NgModule({
   declarations: [AppComponent],
