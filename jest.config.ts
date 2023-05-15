f574bc6090308cfb649e8b4ab3d1bfe645d20584
Gabriel Januário <gabrielojc@gmail.com>
2023-05-15 00:15:07 -0300

    [WMP-0000] Setting up local git hooks

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,20 @@ const config: Config = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: -10,
+    },
+    '*/**': {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: -10,
+    },
+  },
 };
 
 export default config;
